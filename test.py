97e318506d854d3b8e314031a1e1c4b4da40554a
cinarizasyon <kocacinaryazilim@gmail.com>
2019-06-05 23:38:50 +0300

    unit test added.

--- test.py
+++ test.py
@@ -2,29 +2,33 @@ import util
 import colorama
 from termcolor import colored, cprint
 from pynput import keyboard
+import unittest
 
-colorama.init()
 
-test = [
-    [" ","1:s","2","3","4","5","6","7","8"],
-    ["1"," ","F","F","2"," "," "," "," "],
-    ["2","F","F","1","M"," "," "," "," "],
-    ["3"," "," "," "," "," "," "," "," "],
-    ["4"," ","F","F","2","M"," "," "," "],
-    ["5"," ","F","F","2"," "," "," "," "],
-    ["6"," ","F","F","2"," "," "," "," "],
-    ["7"," ","F","F","2"," "," "," "," "],
-    ["8"," ","F","F","2"," "," "," "," "],
-]
+class test_methods(unittest.TestCase):
 
-util.print_table(test)
-def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
+    def test_is_valid_sepetation(self):
+        self.assertTrue(util.is_valid_seperation(2, 2, 8, 8))
 
-# Collect events until releasead
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+    def test_get_neighboord(self):
+        self.assertIsNotNone(util.get_neighboord(1, 1, 8, 8))
 
+        colorama.init()
+
+        test = [
+            [" ", "1:s", "2", "3", "4", "5", "6", "7", "8"],
+            ["1", " ", "F", "F", "2", " ", " ", " ", " "],
+            ["2", "F", "F", "1", "M", " ", " ", " ", " "],
+            ["3", " ", " ", " ", " ", " ", " ", " ", " "],
+            ["4", " ", "F", "F", "2", "M", " ", " ", " "],
+            ["5", " ", "F", "F", "2", " ", " ", " ", " "],
+            ["6", " ", "F", "F", "2", " ", " ", " ", " "],
+            ["7", " ", "F", "F", "2", " ", " ", " ", " "],
+            ["8", " ", "F", "F", "2", " ", " ", " ", " "],
+        ]
+
+        # util.print_table(test)
+
+
+if __name__ == '__main__':
+    unittest.main()
